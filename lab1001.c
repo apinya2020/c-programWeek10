2a97b05bf47a8120a96012fb02e280f30832069b
Mr.Chuchart Saetiew <48706168+apinya2020@users.noreply.github.com>
2020-09-08 12:05:34 +0700

    Add files via upload

---
+++ lab1001.c
@@ -0,0 +1,16 @@
+/*
+    Program:Operator
+    Authority:Mr.chuchart
+    Date:08/09/2560
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "message.h"
+int main(){
+    hello_message();
+    operator1();
+    hello_message();
+    operator2();
+    return 0;
+}
---
+++ message.h
@@ -0,0 +1,7 @@
+#ifndef MESSAGE_H_INCLUDED
+#define MESSAGE_H_INCLUDED
+
+void hello_message();
+int operator1();
+int operator2();
+#endif // MESSAGE_H_INCLUDED
